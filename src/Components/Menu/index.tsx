b958b7160f675fb93eec60763e7ac8ea1af02824
Henrique <henriqueferreira.fox@gmail.com>
2022-11-16 13:29:52 -0300

    add images

--- src/Components/Menu/index.tsx
+++ src/Components/Menu/index.tsx
@@ -1,20 +1,16 @@
 import React, { useEffect, useRef, MutableRefObject } from "react";
 import classes from "./Menu.module.css";
 
-const Menu = () => {
-    const logo = new URL("../../assets/logo.jpg", import.meta.url).href;
+import logo from "../../assets/logo.png";
 
+const Menu = () => {
   return (
     <div className={classes.menu}>
       <ul>
         <li>DÚVIDAS</li>
         <li>CONTATO</li>
       </ul>
-      <img
-        src={logo}
-        className={classes.logo}
-        alt="higheR logo"
-      />
+      <img src={logo} className={classes.logo} alt="higheR logo" />
     </div>
   );
 };
