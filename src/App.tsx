2c0cf646e2aa2f96417963b31f3a794115bed0f1
Henrique <henriqueferreira.fox@gmail.com>
2022-11-19 11:37:38 -0300

    create Video page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./App.css";
 import Index from "./Pages/Index/Index";
 import Acessorios from "./Pages/Acessorios";
 import Mobilidade from "./Pages/Mobilidade";
+import Video from "./Pages/Video";
 
 const App = () => {
   
@@ -14,6 +15,7 @@ const App = () => {
         <Route index path="/" element={<Index />} />
         <Route path="/acessorios" element={<Acessorios />} />
         <Route path="/mobilidade" element={<Mobilidade />} />
+        <Route path="/video" element={<Video />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Components/BackgroundSection/index.tsx
+++ src/Components/BackgroundSection/index.tsx
@@ -3,18 +3,20 @@ import classes from "./BackgroundSection.module.css";
 interface IProps {
   backgroundImageName: string;
   children: React.ReactNode;
+  darkMode?: boolean;
 }
 
 const BackgroundSection: React.FC<IProps> = ({
   backgroundImageName,
   children,
+  darkMode
 }) => {
   const backgroundImage = new URL(
     `../../assets/${backgroundImageName}`,
     import.meta.url
   ).href;
   return (
-    <div className={classes.background}>
+    <div className={`${classes.background} ${darkMode && classes.dark}`}>
       <img src={backgroundImage} />
       {children}
     </div>
