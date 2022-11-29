b43460f4688cc65d2ba0671cc9fbca8162d11464
Henrique <henriqueferreira.fox@gmail.com>
2022-11-29 13:37:46 -0300

    update Acessorios background

--- src/Pages/Acessorios/Sections/Main/index.tsx
+++ src/Pages/Acessorios/Sections/Main/index.tsx
@@ -4,7 +4,7 @@ import Section from "../../../../Components/Section";
 const Main = () => {
   return (
     <Section scrollFadePosition={0}>
-      <BackgroundSection backgroundImageName="background-acessorios.jpg" darkMode={true}>
+      <BackgroundSection backgroundImageName="background-acessorios.jpg" darkMode={false}>
         <h2>HigheR</h2>
         <h1>Acessórios</h1>
       </BackgroundSection>
